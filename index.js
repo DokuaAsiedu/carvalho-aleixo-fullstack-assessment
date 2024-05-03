eba082326781e547977fa1e267b692581480d470
DokuaAsiedu <asiedudokua@gmail.com>
2024-05-03 11:19:30 +0000

    used jsdom to scrape search result data from amazon search url

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require("express");
+const jsdom = require("jsdom");
+
 const app = express();
+const { JSDOM } = jsdom;
+
 const port = 3000;
 
 app.get("/", (req, res) => {
@@ -19,7 +23,30 @@ app.listen(port, () => {
 app.get("/api/scrape", (req, res) => {
 	const { keyword } = req.query;
 
-	if (!keyword.trim()) {
+	if (!keyword || !keyword.trim()) {
 		return res.send("Search keyword should not be null or empty");
 	}
+
+	JSDOM.fromURL(`https://www.amazon.com/s?k=${keyword}`)
+		.then((dom) => {
+			const searchRes = [
+				...dom.window.document.querySelectorAll(
+					".s-result-item:not(.AdHolder)"
+				),
+			]
+				.filter((item) => item.getAttribute("data-asin"))
+				.map((item) => ({
+					imageUrl: item.querySelector(".s-image").src,
+					title: item.querySelector("h2").textContent,
+					rating: item.querySelector("a.a-popover-trigger").textContent,
+					numOfReviews: item.querySelector(
+						"[data-csa-c-content-id='alf-customer-ratings-count-component']"
+					).textContent,
+				}));
+
+			res.send(searchRes);
+		})
+		.catch((err) => {
+			res.send(err);
+		});
 });
