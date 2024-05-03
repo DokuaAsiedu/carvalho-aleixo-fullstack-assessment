2b0f30362840846792db493b192cb557e895c075
DokuaAsiedu <asiedudokua@gmail.com>
2024-05-03 12:08:41 +0000

    added middleware for files/folders in /public folder

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const { JSDOM } = jsdom;
 
 const port = 3000;
 
+app.use(express.static(__dirname + "/public"));
+
 app.get("/", (req, res) => {
 	res.sendFile(__dirname + "/index.html", (err) => {
 		if (err) {
