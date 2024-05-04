cd58f01abf1df798dd27e6f054a237154b6969bd
DokuaAsiedu <asiedudokua@gmail.com>
2024-05-04 17:22:49 +0000

    installed prettier and used formatting on project files

--- index.js
+++ index.js
@@ -1,64 +1,64 @@
-const express = require("express");
-const jsdom = require("jsdom");
+const express = require("express")
+const jsdom = require("jsdom")
 
-const app = express();
-const { JSDOM } = jsdom;
+const app = express()
+const { JSDOM } = jsdom
 
-const port = 3000;
+const port = 3000
 
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/public"))
 
 app.get("/", (req, res) => {
-	res.sendFile(__dirname + "/index.html", (err) => {
-		if (err) {
-			console.log(err);
-			res.render("Please reload the page");
-		} else {
-			console.log("Successfully sent index.html");
-		}
-	});
-});
+  res.sendFile(__dirname + "/index.html", (err) => {
+    if (err) {
+      console.log(err)
+      res.render("Please reload the page")
+    } else {
+      console.log("Successfully sent index.html")
+    }
+  })
+})
 
 app.listen(port, () => {
-	console.log(`Scrapper app listening on port ${port}!`);
-});
+  console.log(`Scrapper app listening on port ${port}!`)
+})
 
 app.get("/api/scrape", (req, res) => {
-	const { keyword } = req.query;
+  const { keyword } = req.query
 
-	if (!keyword || !keyword.trim()) {
-		return res.send("Search keyword should not be null or empty");
-	}
+  if (!keyword || !keyword.trim()) {
+    return res.send("Search keyword should not be null or empty")
+  }
 
-	// use jsdom to get the html from search results page and clean the data to send search results as a list of objects containing data needed for rendering
-	JSDOM.fromURL(`https://www.amazon.com/s?k=${keyword}`)
-		.then((dom) => {
-			const searchRes = [
-				...dom.window.document.querySelectorAll(
-					"[data-component-type='s-search-result']:not(.AdHolder)"
-				),
-			]
-				.filter((item) => item.getAttribute("data-asin"))
-				.map((item) => {
-					const textArr = item.textContent.split("  ");
-					const title = textArr[0];
-					const [rating, numOfReviews] = textArr[1].split(" out of 5 stars ");
-					return {
-						imageUrl: item.querySelector(".s-image").src,
-						imageAlt: item.querySelector(".s-image").alt,
-						imageUrlSet: item.querySelector(".s-image").srcset,
-						title,
-						rating,
-						numOfReviews,
-					};
-				});
+  // use jsdom to get the html from search results page and clean the data to send search results as a list of objects containing data needed for rendering
+  JSDOM.fromURL(`https://www.amazon.com/s?k=${keyword}`)
+    .then((dom) => {
+      const searchRes = [
+        ...dom.window.document.querySelectorAll(
+          "[data-component-type='s-search-result']:not(.AdHolder)"
+        ),
+      ]
+        .filter((item) => item.getAttribute("data-asin"))
+        .map((item) => {
+          const textArr = item.textContent.split("  ")
+          const title = textArr[0]
+          const [rating, numOfReviews] = textArr[1].split(" out of 5 stars ")
+          return {
+            imageUrl: item.querySelector(".s-image").src,
+            imageAlt: item.querySelector(".s-image").alt,
+            imageUrlSet: item.querySelector(".s-image").srcset,
+            title,
+            rating,
+            numOfReviews,
+          }
+        })
 
-			// console.log(searchRes);
+      // console.log(searchRes);
 
-			res.send(searchRes);
-		})
-		.catch((err) => {
-			res.send(err);
-			console.log(err);
-		});
-});
+      res.send(searchRes)
+    })
+    .catch((err) => {
+      res.send(err)
+      console.log(err)
+    })
+})
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./index.html"],
-	theme: {
-		extend: {
-			container: {
-				center: true,
-				padding: {
-					DEFAULT: "1rem",
-					md: "2rem",
-					lg: "3rem",
-				},
-			},
-		},
-	},
-	plugins: [],
-};
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          md: "2rem",
+          lg: "3rem",
+        },
+      },
+    },
+  },
+  plugins: [],
+}
